ec619b2878f1bbe6c034e4d2c295896ea3de62cf
Matheus Anjos <168678938+MatheusAnjoss@users.noreply.github.com>
2025-04-12 20:23:26 -0300

    Update funcoes.php

--- Trabalho PHP - Karin e Matheus/pokemon-catalog/src/utils/funcoes.php
+++ Trabalho PHP - Karin e Matheus/pokemon-catalog/src/utils/funcoes.php
@@ -1,10 +1,25 @@
 <?php
-function filterPokemonsByType($pokemons, $type) {
-    return array_filter($pokemons, function ($pokemon) use ($type) {
-        return !$type || stripos($pokemon['type'], $type) !== false;
-    });
-}
-
 function sanitizeInput($input) {
     return htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8');
+}
+
+function isValidUrl($url) {
+    return filter_var($url, FILTER_VALIDATE_URL) && preg_match('/^https:\/\//', $url);
+}
+
+function isValidPositiveInteger($number) {
+    return is_numeric($number) && (int)$number > 0;
+}
+
+function isValidValue($value, $validValues) {
+    return in_array($value, $validValues, true);
+}
+
+function pokemonExists($pokemons, $name, $pokedexNumber) {
+    foreach ($pokemons as $pokemon) {
+        if ($pokemon['name'] === $name || $pokemon['pokedex_number'] == $pokedexNumber) {
+            return true;
+        }
+    }
+    return false;
 }
